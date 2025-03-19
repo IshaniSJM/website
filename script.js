8c3ab8cd4a9ac470cbb3676bd6071a42a997608d
Ishani Sadagopan Jeyaram Malismitha <ishanisadagopan04jm@gmail.com>
2025-03-18 19:00:38 -0500

    Basic style of website

---
+++ script.js
@@ -0,0 +1,9 @@
+const main = document.getElementById("main");
+    const navbar = document.getElementById("navbar");
+        const abtMe = document.getElementById("abtMe");
+        const projects = document.getElementById("projects");
+        const contacts = document.getElementById("contacts");
+
+    const content = document.getElementById("content");
+
+    const footer = document.getElementById("footer");
